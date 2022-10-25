cf0a7672b073cf277ef0d68c9cd76a717a638736
LegoLeo7 <99499098+LegoLeo7@users.noreply.github.com>
2022-10-25 19:08:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+
+function preload() {
+
+ }
+
+ function setup() 
+ { canvas = createCanvas(300, 300); }
+
+ function draw() {
+
+  }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
